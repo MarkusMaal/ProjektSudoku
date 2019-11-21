8062a92070f7e21e7927bb9929786e0ac84aa241
MarkusMaal <markus.maal@gmail.com>
2019-11-21 12:11:00 +0200

    3x3 alade genereerimine

--- Mäng.py
+++ Mäng.py
@@ -2,35 +2,43 @@ from random import randint
 
 # loogika testimine
 arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-rida = []
+
+blokk = []
+blokid = []
+
 read = []
+rida = []
+
+
+def CheckBlock(x, blokid):
+    isin = False
+    for b in blokid:
+        for subb in b:
+            if subb == x:
+                isin = True
+    return isin
+
+
 for i in range(9):
-    arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    rida = []
-    for i in range(9):
-        valik = randint(0, len(arvud) - 1)
-        good = False
-        while not good:
-            if len(read) - 1 >= i:
-                # kontrolli arve horisontaalselt
-                for x in range(len(read[i])):
-                    good = True
-                    if read[i][x] == valik:
-                        valik = randint(0, len(arvud) - 1)
-                        good = False
-                        break
-            # kontrolli arve vertikaalselt
-            for j in range(len(read)):
-                good = True
-                if read[j][i] == valik:
-                    valik = randint(0, len(arvud) - 1)
-                    good = False
-                    break
-            if len(read) == 0:
-                good = True
+    if i == 3 or i == 6: arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for d in range(3):
+        blokk = []
+        while len(blokk) < 3:
+            if len(arvud) == 0:
                 break
-        rida.append(arvud[valik])
-        del arvud[valik]
-    read.append(rida)
-for rida in read:
-    print(rida)
+            e = randint(0, len(arvud) - 1)
+            valik = arvud[e]
+            blokk.append(valik)
+            arvud.remove(valik)
+        blokid.append(blokk)
+
+"""for h in range(9):
+    rida.append(arvud[valik])
+    del arvud[valik]
+    read.append(rida)"""
+print("Sudoku backend test")
+uusb = []
+for b in blokid:
+    if not b == []:
+        uusb.append(b)
+print(uusb)
