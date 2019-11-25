b28bd4890a45cb37206af8083b4c7077525b77eb
MarkusMaal <markus.maal@gmail.com>
2019-11-25 16:44:28 +0200

    3x3 alade genereerimine, hetkel pole täielikult funktsionaalne

--- Mäng.py
+++ Mäng.py
@@ -6,6 +6,7 @@ arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 blokk = []
 blokid = []
 
+
 read = []
 rida = []
 
@@ -19,7 +20,25 @@ def CheckBlock(x, blokid):
     return isin
 
 
-for i in range(9):
+def CheckGrid(kontroll, valik, positsioon):
+    # vali välja millist rida kontrollida
+    y = positsioon[1]
+    complist = kontroll[y]
+    print(complist)
+    tagasta = False
+    for item in complist:
+        if item == valik:
+            tagasta = True
+    return tagasta
+
+# sektsioonidesse lisatakse 3x3 2d listidena
+
+
+sektsioonid = []
+
+print("Sudoku backend test")
+
+for i in range(3):
     if i == 3 or i == 6: arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     for d in range(3):
         blokk = []
@@ -36,9 +55,51 @@ for i in range(9):
     rida.append(arvud[valik])
     del arvud[valik]
     read.append(rida)"""
-print("Sudoku backend test")
 uusb = []
 for b in blokid:
     if not b == []:
         uusb.append(b)
-print(uusb)
+sektsioonid.append(uusb)
+
+for x in range(2):
+    kontroll = sektsioonid[-1]
+    fails = 0
+    arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in range(3):
+        test = 0
+        for d in range(3):
+            blokk = []
+            while len(blokk) < 3:
+                if len(arvud) == 0:
+                    break
+                e = test
+                if len(arvud) - 1 >= e:
+                    valik = arvud[e]
+                if fails > len(arvud):
+                    fails = 0
+                    valik = 0
+                if CheckGrid(kontroll, valik, [d, i]):
+                    test += 1
+                    if test > len(arvud) - 1:
+                        test = 0
+                        fails += 1
+                    continue
+                else:
+                    if len(arvud) == 0:
+                        break
+                    else:
+                        blokk.append(valik)
+                        if valik in arvud: arvud.remove(valik)
+            blokid.append(blokk)
+
+    """for h in range(9):
+        rida.append(arvud[valik])
+        del arvud[valik]
+        read.append(rida)"""
+    uusb = []
+    for b in blokid:
+        if not b == []:
+            if not b in sektsioonid[-1]:
+                uusb.append(b)
+    sektsioonid.append(uusb)
+print(sektsioonid)
