05cabd627e23fa1cf60a3edbaf0c42f0c1ad8dcb
MarkusMaal <markus.maal@gmail.com>
2019-12-16 17:10:55 +0200

    3x3 alade genereerimine, hetkel pole täielikult funktsionaalne

--- Mäng.py
+++ Mäng.py
@@ -1,105 +1,77 @@
 from random import randint
 
+
+
+
 # loogika testimine
 arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 blokk = []
-blokid = []
+# üks rida muutujas tabel tähistab ühte gruppi
+# tärn on grupi eraldaja
+# ... on ühe rea gruppe eraldaja
+tabel = "521 467 000*" \
+        "000 000 000*" \
+        "000 000 000*" \
+        "..." \
+        "000 000 000*" \
+        "000 000 000*" \
+        "000 000 000*" \
+        "..." \
+        "000 000 000*" \
+        "000 000 000*" \
+        "000 000 000*"
+
+clusterid = tabel.split("...")
 
 
 read = []
 rida = []
 
 
-def CheckBlock(x, blokid):
-    isin = False
-    for b in blokid:
-        for subb in b:
-            if subb == x:
-                isin = True
-    return isin
-
-
-def CheckGrid(kontroll, valik, positsioon):
-    # vali välja millist rida kontrollida
-    y = positsioon[1]
-    complist = kontroll[y]
-    print(complist)
-    tagasta = False
-    for item in complist:
-        if item == valik:
-            tagasta = True
-    return tagasta
-
-# sektsioonidesse lisatakse 3x3 2d listidena
-
-
-sektsioonid = []
-
-print("Sudoku backend test")
-
-for i in range(3):
-    if i == 3 or i == 6: arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for d in range(3):
-        blokk = []
-        while len(blokk) < 3:
-            if len(arvud) == 0:
-                break
-            e = randint(0, len(arvud) - 1)
-            valik = arvud[e]
-            blokk.append(valik)
-            arvud.remove(valik)
-        blokid.append(blokk)
-
-"""for h in range(9):
-    rida.append(arvud[valik])
-    del arvud[valik]
-    read.append(rida)"""
-uusb = []
-for b in blokid:
-    if not b == []:
-        uusb.append(b)
-sektsioonid.append(uusb)
-
-for x in range(2):
-    kontroll = sektsioonid[-1]
-    fails = 0
-    arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def CreateSection(count, arvud, empties, eelmine, ülemine):
+    s = ""
+    eelread = eelmine.split("\n")
+    üleread = ülemine.split("\n")
+    ülekolonnid = []
     for i in range(3):
-        test = 0
-        for d in range(3):
-            blokk = []
-            while len(blokk) < 3:
-                if len(arvud) == 0:
-                    break
-                e = test
-                if len(arvud) - 1 >= e:
-                    valik = arvud[e]
-                if fails > len(arvud):
-                    fails = 0
-                    valik = 0
-                if CheckGrid(kontroll, valik, [d, i]):
-                    test += 1
-                    if test > len(arvud) - 1:
-                        test = 0
-                        fails += 1
-                    continue
-                else:
-                    if len(arvud) == 0:
-                        break
-                    else:
-                        blokk.append(valik)
-                        if valik in arvud: arvud.remove(valik)
-            blokid.append(blokk)
+        ülekolonnid.append(üleread[0][i] + üleread[1][i] + üleread[2][i])
+    rida = 1
+    for i in range(empties):
+        arvud.append(0)
+    for i in range(count):
+        if len(s) > 3: s = ""
+        teststring = eelread[0]
+        test = randint(0, len(arvud) - 1)
+        while (teststring in eelread[rida - 1]) or (teststring in ülekolonnid[rida - 1]):
+            test = randint(0, len(arvud) - 1)
+            teststring = str(arvud[test])
+            print(teststring)
+        s += teststring
+        arvud.remove(arvud[test])
+        if i == 2 or i == 5:
+            rida += 1
+            s += "\n"
+    return s
 
-    """for h in range(9):
-        rida.append(arvud[valik])
-        del arvud[valik]
-        read.append(rida)"""
-    uusb = []
-    for b in blokid:
-        if not b == []:
-            if not b in sektsioonid[-1]:
-                uusb.append(b)
-    sektsioonid.append(uusb)
-print(sektsioonid)
+def CheckBlock(location, clusterid):
+    isin = False
+    ly = 1
+    for cluster in clusterid:
+        for group in cluster.split("*"):
+            print(group)
+
+clusterid = []
+eelmine = "000\n000\n000"
+for d in range(3):
+    sektsioonid = []
+    for i in range(3):
+        if d == 0:
+            ülemine = "000\n000\n000"
+        else:
+            ülemine = clusterid[-1][i]
+        sektsioonid.append(CreateSection(9, arvud, 4, eelmine, ülemine))
+        eelmine = sektsioonid[-1]
+        arvud = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    clusterid.append(sektsioonid)
+print(clusterid)
