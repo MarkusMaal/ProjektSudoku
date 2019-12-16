44ebb98757e59cecd24e68a135f1c0b09488baac
jan10markus <56549698+jan10markus@users.noreply.github.com>
2019-12-16 17:40:35 +0200

    Add files via upload

--- Joonistamine.py
+++ Joonistamine.py
@@ -4,18 +4,18 @@ import time
 
 raam = Tk()
 raam.title("Sudoku")
-raam.geometry("1080x480")
+raam.geometry("700x500")
 
-tahvel = Canvas(raam, width=900, height=900, background="white")
+tahvel = Canvas(raam, width=500, height=500, background="white")
 tahvel.grid()
-
 
 #joonistab paksemad jooned
 def DrawLines():
-    tahvel.create_line(0, 163, 500, 163, width=3, fill="black")
-    tahvel.create_line(0, 328, 500, 328, width=3, fill="black")
-    tahvel.create_line(163, 0, 163, 500, width=3, fill="black")
-    tahvel.create_line(328, 0, 328, 500, width=3, fill="black")
+    tahvel.create_rectangle(0, 0, 500, 500, fill="black")
+    tahvel.create_line(0, 163, 500, 163, width=3, fill="white")
+    tahvel.create_line(0, 328, 500, 328, width=3, fill="white")
+    tahvel.create_line(163, 0, 163, 500, width=3, fill="white")
+    tahvel.create_line(328, 0, 328, 500, width=3, fill="white")
 
 #klass, milles üks objekt on 1 ruut
 class Cell():
@@ -24,20 +24,47 @@ class Cell():
         self.y = y
         self.value = value
 
+    #joonistab ruudu ja sisse numbri
     def DrawCell(self):
+        self.joonis = tahvel.create_rectangle(self.x, self.y, self.x+50, self.y+50, fill="white", outline="black", width=1, activefill="lightblue")
         self.number = tahvel.create_text(self.x + 25, self.y + 25, text=str(self.value))
-        self.joonis = tahvel.create_rectangle(self.x, self.y, self.x+50, self.y+50, outline="black", width=1, activefill="lightblue")
+        tahvel.tag_bind(self.joonis, '<1>', self.clicked)
+        tahvel.tag_bind(self.number, '<1>', self.clicked)
+
+    #määrab numbri, mis cell'i/ruudu eelnevale numbrile asemele panna
+    def keypress(self, event=None):
+        if int(event.char) > 0 and int(event.char) < 10:
+            self.value = event.char
+            tahvel.delete(self.number)
+            tahvel.delete(self.joonis)
+            Cell.DrawCell(self)
+
+    #teeb koha "..."'iks, ootab sisendit kasutajalt
+    def clicked(self, event=None):
+        print("clicked on", self.value)
+        tahvel.delete(self.number)
+        self.number = tahvel.create_text(self.x + 25, self.y + 25, text="...")
+        tahvel.tag_bind(self.number, '<1>', self.clicked)
+        raam.bind("<Key>", self.keypress)
 
 cells = []
-DrawLines()
 
+#loob Cell() objektid
 for i in range(0, 9):
     for j in range(0, 9):
-        cell = Cell(j*55, i*55, "")
+        cell = Cell(j*55, i*55, " ")
         cells.append(cell)
-        #cell.value = cells.index(cell)
 
-for i in cells:
-    Cell.DrawCell(i)
+#joonistab ruudustiku
+def Draw():
+    DrawLines()
+    for i in cells:
+        Cell.DrawCell(i)
+    uus_mäng = Button(raam, text="Uus mäng")
+    uus_mäng.grid(row=0, column=1)
+    kontrolli = Button(raam, text="Kontrolli")
+    kontrolli.grid(row=0, column=2)
+
+Draw()
 
 raam.mainloop()
