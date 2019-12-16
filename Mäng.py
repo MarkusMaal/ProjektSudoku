e696154ce4df7d7bce075941c45fc7f2d4376fba
MarkusMaal <markus.maal@gmail.com>
2019-12-16 17:39:40 +0200

    3x3 alade genereerimine, hetkel pole täielikult funktsionaalne

--- Mäng.py
+++ Mäng.py
@@ -95,8 +95,9 @@ def CreateSection(count, arvud, empties, eelmine = "000\n000\n000", ülemine = "
             s += "\n"
     return s
 
+
 rida1 = [Grid(), Grid(), Grid()]
-for i in range(len(rida1)):
+for i in range(3):
     rida1[i].Defineeri(CreateSection(9, arvud, 3))
     rida1[i].ToString()
 print(rida1[0].Kontrolli(0, 0, 5))
