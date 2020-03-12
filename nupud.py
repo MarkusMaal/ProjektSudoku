77a9c85c6fad253b352d6333108b7045fa91d906
jan10markus <56549698+jan10markus@users.noreply.github.com>
2020-03-12 16:09:47 +0200

    button placement

---
+++ nupud.py
@@ -0,0 +1,8 @@
+uus_mäng = Button(raam, text="Uus mäng")
+uus_mäng.place(x=525, y=200)
+raskusaste_label = Label(raam, text ="Raskusaste:")
+raskusaste_label.place(x=525, y=300)
+raskusaste = Entry(raam, width=2)
+raskusaste.place(x=600, y=300)
+kontrolli = Button(raam, text="Kontrolli")
+kontrolli.place(x=525, y=250)
